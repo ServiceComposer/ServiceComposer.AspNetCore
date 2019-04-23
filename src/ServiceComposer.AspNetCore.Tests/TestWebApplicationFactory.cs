8b711e928ebdc3ddb112b5620a90484d5f5151e8
Mauro Servienti <mauro.servienti@gmail.com>
2019-04-23 15:51:28 +0200

    self-contained tests using custom WebApplicationfactory

---
+++ src/ServiceComposer.AspNetCore.Tests/TestWebApplicationFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ServiceComposer.AspNetCore.Tests
+{
+    public class TestWebApplicationFactory<TEntryPoint> :
+        WebApplicationFactory<TEntryPoint>
+        where TEntryPoint : class
+    {
+        protected override IWebHostBuilder CreateWebHostBuilder()
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<TEntryPoint>();
+
+            return host;
+        }
+    }
+}
