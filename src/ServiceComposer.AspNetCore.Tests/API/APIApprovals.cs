8a4531d50c3a51fbec3b9bf8614fd8a333e7fadf
Mauro Servienti <mauro.servienti@gmail.com>
2019-04-24 10:50:50 +0200

    Add API Approvals

---
+++ src/ServiceComposer.AspNetCore.Tests/API/APIApprovals.cs
@@ -0,0 +1,19 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using PublicApiGenerator;
+using Xunit;
+
+namespace ServiceComposer.AspNetCore.Tests.API
+{
+    public class APIApprovals
+    {
+        [Fact]
+        [UseReporter(typeof(DiffReporter))]
+        public void Approve_API()
+        {
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(IInterceptRoutes).Assembly);
+
+            Approvals.Verify(publicApi);
+        }
+    }
+}
