ad3e1933b713690f8ab80aac47dc95dbdf620133
Mauro Servienti <mauro.servienti@gmail.com>
2019-04-24 10:53:47 +0200

    disable inlining to prevent API Approvals to fail wen in release mode

--- src/ServiceComposer.AspNetCore.Tests/API/APIApprovals.cs
+++ src/ServiceComposer.AspNetCore.Tests/API/APIApprovals.cs
@@ -1,6 +1,7 @@
 ﻿using ApprovalTests;
 using ApprovalTests.Reporters;
 using PublicApiGenerator;
+using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace ServiceComposer.AspNetCore.Tests.API
@@ -9,6 +10,7 @@ namespace ServiceComposer.AspNetCore.Tests.API
     {
         [Fact]
         [UseReporter(typeof(DiffReporter))]
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public void Approve_API()
         {
             var publicApi = ApiGenerator.GeneratePublicApi(typeof(IInterceptRoutes).Assembly);
