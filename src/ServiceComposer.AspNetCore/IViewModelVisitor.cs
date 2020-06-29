0d1e12f337a9757316d86f72c98801c5e7a35a27
mauroservienti <mauro.servienti@gmail.com>
2020-06-29 17:03:55 +0200

    Add support for ViewModel visitors

---
+++ src/ServiceComposer.AspNetCore/IViewModelVisitor.cs
@@ -0,0 +1,13 @@
+﻿#if NETCOREAPP3_1
+
+using System.Threading.Tasks;
+
+namespace ServiceComposer.AspNetCore
+{
+    public interface IViewModelVisitor
+    {
+        Task Visit(dynamic viewModel);
+    }
+}
+
+#endif
