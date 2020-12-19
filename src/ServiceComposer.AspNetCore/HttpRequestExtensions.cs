d38f70ae3acdeb3b1d7558dab47b0bf2c86de0e9
Mauro Servienti <mauro.servienti@gmail.com>
2020-12-19 19:11:04 +0100

    Use composition context within DynamicViewModel

--- src/ServiceComposer.AspNetCore/HttpRequestExtensions.cs
+++ src/ServiceComposer.AspNetCore/HttpRequestExtensions.cs
@@ -7,20 +7,26 @@ namespace ServiceComposer.AspNetCore
     public static class HttpRequestExtensions
     {
         internal static readonly string ComposedResponseModelKey = "composed-response-model";
+        internal static readonly string CompositionContextKey = "composition-context";
 
         public static dynamic GetComposedResponseModel(this HttpRequest request)
         {
             return request.HttpContext.Items[ComposedResponseModelKey];
         }
 
-        internal static void SetModel(this HttpRequest request, dynamic viewModel)
+        internal static void SetViewModel(this HttpRequest request, dynamic viewModel)
         {
             request.HttpContext.Items.Add(HttpRequestExtensions.ComposedResponseModelKey, viewModel);
         }
         
+        internal static void SetCompositionContext(this HttpRequest request, ICompositionContext compositionContext)
+        {
+            request.HttpContext.Items.Add(HttpRequestExtensions.CompositionContextKey, compositionContext);
+        }
+        
         public static ICompositionContext GetCompositionContext(this HttpRequest request)
         {
-            return (ICompositionContext)request.HttpContext.Items[ComposedResponseModelKey];
+            return (ICompositionContext)request.HttpContext.Items[CompositionContextKey];
         }
     }
 }
