9c24d3b4b2818a4c881e39ee70102ea0dd2d7e74
Mauro Servienti <mauro.servienti@gmail.com>
2020-12-19 15:30:39 +0100

    log obsolete message test

---
+++ src/ServiceComposer.AspNetCore.Tests/When_raising_event_using_DynamicViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FakeItEasy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace ServiceComposer.AspNetCore.Tests
+{
+    public class When_raising_event_using_DynamicViewModel
+    {
+        class Logger : ILogger<DynamicViewModel>
+        {
+            public LogLevel Level { get; set; }
+            public string Message { get; set; }
+            
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                Level = logLevel;
+                Message = state.ToString();
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        [Fact]
+        public async Task Should_log_warning()
+        {
+            // Arrange
+            var logger = new Logger();
+            dynamic sut = new DynamicViewModel(logger,"empty", A.Fake<RouteData>(), A.Fake<HttpRequest>());
+
+            await sut.RaiseEvent(new object());
+            
+            Assert.Equal(LogLevel.Warning, logger.Level);
+            Assert.Equal("dynamic.RaiseEvent is obsolete. It'll be treated as an error starting v2 and removed in v3. Use HttpRequest.GetCompositionContext() to raise events.", logger.Message);
+        }
+    }
+}
