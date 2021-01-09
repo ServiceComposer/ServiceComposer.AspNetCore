a4ea57971197ead5de133e8791ce27c975f015e2
Mauro Servienti <mauro.servienti@gmail.com>
2021-01-09 20:44:53 +0100

    View model factory snippets

---
+++ src/Snippets.NetCore3x/ViewModelFactory/MarketingProductInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceComposer.AspNetCore;
+
+namespace Snippets.NetCore3x.ViewModelFactory
+{
+    // begin-snippet: view-model-factory-marketing-handler
+    public class MarketingProductInfo: ICompositionRequestsHandler
+    {
+        [HttpGet("/product/{id}")]
+        public Task Handle(HttpRequest request)
+        {
+            var vm = request.GetComposedResponseModel<ProductViewModel>();
+
+            //retrieve product details from the marketing database or service
+            vm.ProductName = "Sample product";
+            vm.ProductDescription = "This is a sample product";
+            
+            return Task.CompletedTask;
+        }
+    }
+    // end-snippet
+}
