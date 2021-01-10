866f1dced544211081057413b1a7e6fae9055bcc
Mauro Servienti <mauro.servienti@gmail.com>
2021-01-10 15:08:26 +0100

    net-core-3x-default-casing snippet

---
+++ src/Snippets.NetCore3x/DefaultCasing/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ServiceComposer.AspNetCore;
+
+namespace Snippets.NetCore3x.DefaultCasing
+{
+    public class Startup
+    {
+        // begin-snippet: net-core-3x-default-casing
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+            services.AddViewModelComposition(options =>
+            {
+                options.ResponseSerialization.DefaultResponseCasing = ResponseCasing.PascalCase;
+            });
+            // end-snippet
+        }
+    }
+}
