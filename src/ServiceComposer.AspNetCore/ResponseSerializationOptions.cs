4d37f5e4c2334160979ef2c20493919c289719b7
mauroservienti <mauro.servienti@gmail.com>
2021-03-04 17:11:31 +0100

    Add options to customize serialization settings

--- src/ServiceComposer.AspNetCore/ResponseSerializationOptions.cs
+++ src/ServiceComposer.AspNetCore/ResponseSerializationOptions.cs
@@ -1,9 +1,26 @@
-﻿namespace ServiceComposer.AspNetCore
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace ServiceComposer.AspNetCore
 {
 #if NETCOREAPP3_1 || NET5_0
     public class ResponseSerializationOptions
     {
+        private readonly IServiceCollection services;
+
+        public ResponseSerializationOptions(IServiceCollection services)
+        {
+            this.services = services;
+        }
+
         public ResponseCasing DefaultResponseCasing { get; set; } = ResponseCasing.CamelCase;
+
+        public void UseCustomJsonSerializerSettings(Func<HttpRequest, JsonSerializerSettings> jsonSerializerSettingsConfig)
+        {
+            services.AddSingleton(jsonSerializerSettingsConfig);
+        }
     }
 
     public enum ResponseCasing
