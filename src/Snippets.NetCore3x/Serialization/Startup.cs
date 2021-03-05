3ebb9e4de6b52596451a4176d0c01ed17ee53a4b
mauroservienti <mauro.servienti@gmail.com>
2021-03-05 12:13:17 +0100

    net-core-3x-custom-serialization-settings

---
+++ src/Snippets.NetCore3x/Serialization/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using ServiceComposer.AspNetCore;
+
+namespace Snippets.NetCore3x.Serialization
+{
+    public class Startup
+    {
+        // begin-snippet: net-core-3x-custom-serialization-settings
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+            services.AddViewModelComposition(options =>
+            {
+                options.ResponseSerialization.UseCustomJsonSerializerSettings(request =>
+                {
+                    return new JsonSerializerSettings();
+                });
+            });
+        }
+        // end-snippet
+    }
+}
