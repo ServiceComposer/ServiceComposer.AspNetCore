50510588b3405a302de4576e87a35322f585c1fb
Mauro Servienti <mauro.servienti@gmail.com>
2021-03-09 05:02:17 +0100

    move binding logic into its own class

--- src/ServiceComposer.AspNetCore.Endpoints.Tests/Post_with_2_handlers.cs
+++ src/ServiceComposer.AspNetCore.Endpoints.Tests/Post_with_2_handlers.cs
@@ -204,4 +204,72 @@ namespace ServiceComposer.AspNetCore.Endpoints.Tests
     {
         public string AString { get; set; }
     }
+
+    class Binder
+    {
+        IModelBinderFactory modelBinderFactory;
+        IModelMetadataProvider modelMetadataProvider;
+        IOptions<MvcOptions> mvcOptions;
+
+        public Binder(IModelBinderFactory modelBinderFactory, IModelMetadataProvider modelMetadataProvider, IOptions<MvcOptions> mvcOptions)
+        {
+            this.modelBinderFactory = modelBinderFactory;
+            this.modelMetadataProvider = modelMetadataProvider;
+            this.mvcOptions = mvcOptions;
+        }
+
+        public async Task<T> Bind<T>(HttpRequest request) where T : new()
+        {
+            //always rewind the stream; otherwise,
+            //if multiple handler concurrently bind
+            //different models only the first one succeeds
+            request.Body.Position = 0;
+
+            var modelType = typeof(T);
+            var modelMetadata = modelMetadataProvider.GetMetadataForType(modelType);
+            var actionContext = new ActionContext(
+                request.HttpContext,
+                request.HttpContext.GetRouteData(),
+                new ActionDescriptor(),
+                new ModelStateDictionary());
+            var valueProvider =
+                await CompositeValueProvider.CreateAsync(actionContext, mvcOptions.Value.ValueProviderFactories);
+
+#if NET5_0
+            if (modelMetadata.BoundConstructor != null)
+            {
+                throw new NotSupportedException("Record type not supported");
+            }
+#endif
+
+            var modelBindingContext = DefaultModelBindingContext.CreateBindingContext(
+                actionContext,
+                valueProvider,
+                modelMetadata,
+                bindingInfo: null,
+                modelName: "");
+
+            modelBindingContext.Model = new T();
+            modelBindingContext.PropertyFilter = _ => true; // All props
+
+            var factoryContext = new ModelBinderFactoryContext()
+            {
+                Metadata = modelMetadata,
+                BindingInfo = new BindingInfo()
+                {
+                    BinderModelName = modelMetadata.BinderModelName,
+                    BinderType = modelMetadata.BinderType,
+                    BindingSource = modelMetadata.BindingSource,
+                    PropertyFilterProvider = modelMetadata.PropertyFilterProvider,
+                },
+                CacheToken = modelMetadata,
+            };
+
+            await modelBinderFactory
+                .CreateBinder(factoryContext)
+                .BindModelAsync(modelBindingContext);
+
+            return (T) modelBindingContext.Result.Model;
+        }
+    }
 }
