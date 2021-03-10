c6db1969efc0a5e441bccc072446e4929cb3dfbd
mauroservienti <mauro.servienti@gmail.com>
2021-03-10 11:36:27 +0100

    net-core-3x-use-output-formatters snippet

---
+++ src/Snippets.NetCore3x/Serialization/UseOutputFormatters.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ServiceComposer.AspNetCore;
+
+namespace Snippets.NetCore3x.Serialization
+{
+    public class UseOutputFormatters
+    {
+        // begin-snippet: net-core-3x-use-output-formatters
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddViewModelComposition(options =>
+            {
+                options.ResponseSerialization.UseOutputFormatters = true;
+            });
+            services.AddControllers();
+        }
+
+        // end-snippet
+    }
+}
