12e91ca2e781dd04d5f90be0d83bf5ccd82b01b7
mauroservienti <mauro.servienti@gmail.com>
2021-03-11 16:56:15 +0100

    snippet: net-core-3x-model-binding-raw-body-usage

---
+++ src/Snippets.NetCore3x/ModelBinding/RawBodyUsageHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using ServiceComposer.AspNetCore;
+
+namespace Snippets.NetCore3x.ModelBinding
+{
+    class RawBodyUsageHandler : ICompositionRequestsHandler
+    {
+        // begin-snippet: net-core-3x-model-binding-raw-body-usage
+        [HttpPost("/sample/{id}")]
+        public async Task Handle(HttpRequest request)
+        {
+            request.Body.Position = 0;
+            using var reader = new StreamReader(request.Body, Encoding.UTF8, leaveOpen: true );
+            var body = await reader.ReadToEndAsync();
+            var content = JObject.Parse(body);
+
+            var vm = request.GetComposedResponseModel();
+            vm.AString = content?.SelectToken("AString")?.Value<string>();
+        }
+        // end-snippet
+    }
+}
