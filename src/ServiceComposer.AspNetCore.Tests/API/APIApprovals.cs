7eb9ffed4124f4e8989efe0eeef31e0302a76818
Mauro Servienti <mauro.servienti@gmail.com>
2021-12-07 21:55:45 +0100

    Simplify API approvals

--- src/ServiceComposer.AspNetCore.Tests/API/APIApprovals.cs
+++ src/ServiceComposer.AspNetCore.Tests/API/APIApprovals.cs
@@ -12,18 +12,12 @@ namespace ServiceComposer.AspNetCore.Tests.API
         [Fact]
         [UseReporter(typeof(DiffReporter))]
         [MethodImpl(MethodImplOptions.NoInlining)]
-#if NETCOREAPP3_1
-        [UseApprovalSubdirectory("NETCOREAPP3_1")]
-#endif
-#if NET5_0
-        [UseApprovalSubdirectory("NET5_0")]
-#endif
-#if NET6_0
-        [UseApprovalSubdirectory("NET6_0")]
-#endif
         public void Approve_API()
         {
-            var publicApi = typeof(IInterceptRoutes).Assembly.GeneratePublicApi();
+            var publicApi = typeof(IInterceptRoutes).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+            {
+                ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+            });
 
             Approvals.Verify(publicApi);
         }
