3e4e25aa48e1b69d1b76406c05c7b3e961ad8cad
Mauro Servienti <mauro.servienti@gmail.com>
2023-06-11 21:46:35 +0200

    snippet: scatter-gather-gather-override

---
+++ src/Snippets/ScatterGather/GatherMethodOverride.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using ServiceComposer.AspNetCore;
+
+namespace Snippets.ScatterGather;
+
+public class GatherMethodOverride
+{
+    // begin-snippet: scatter-gather-gather-override
+    public class CustomGatherer : Gatherer
+    {
+        public CustomGatherer(string key, string destination) : base(key, destination) { }
+
+        public override Task<IEnumerable<JsonNode>> Gather(HttpContext context)
+        {
+            // by overriding this method we can implement custom logic
+            // to gather the responses from the downstream service.
+            return base.Gather(context);
+        }
+    }
+    // end-snippet
+}
