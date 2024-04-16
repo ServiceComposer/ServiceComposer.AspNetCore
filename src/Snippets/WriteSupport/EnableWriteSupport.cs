5368d1de1bb248869859ebbdc84448dcfa9ea6a2
Mauro Servienti <mauro.servienti@gmail.com>
2024-04-16 14:16:25 +0200

    Add a disable write support snippet

--- src/Snippets/WriteSupport/EnableWriteSupport.cs
+++ src/Snippets/WriteSupport/EnableWriteSupport.cs
@@ -5,10 +5,22 @@ namespace Snippets.NetCore3x.WriteSupport;
 
 public class EnableWriteSupport
 {
+#pragma warning disable CS0618 // Type or member is obsolete
     // begin-snippet: enable-write-support
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddViewModelComposition(options => options.EnableWriteSupport());
+    } 
+    // end-snippet
+#pragma warning restore CS0618 // Type or member is obsolete
+}
+
+public class DisableWriteSupport
+{
+    // begin-snippet: disable-write-support
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddViewModelComposition(options => options.DisableWriteSupport());
     } 
     // end-snippet
 }
