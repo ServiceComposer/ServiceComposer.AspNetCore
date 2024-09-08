f81560afc64b50766fbc0b73e925953341478b67
Mauro Servienti <mauro.servienti@gmail.com>
2024-09-08 09:49:22 +0200

    Remove commented code

--- src/ServiceComposer.AspNetCore/CompositionEndpointBuilder.cs
+++ src/ServiceComposer.AspNetCore/CompositionEndpointBuilder.cs
@@ -193,52 +193,5 @@ namespace ServiceComposer.AspNetCore
 
             return routeEndpoint;
         }
-        
-#if NET8_0
-        // static RequestDelegate Create(RequestDelegate requestDelegate, RequestDelegateFactoryOptions options)
-        // {
-        //     var serviceProvider = options.ServiceProvider ?? options.EndpointBuilder.ApplicationServices;
-        //     var jsonOptions = serviceProvider?.GetService<IOptions<JsonOptions>>()?.Value ?? new JsonOptions();
-        //     var jsonSerializerOptions = jsonOptions.SerializerOptions;
-        //
-        //     var factoryContext = new EndpointFilterFactoryContext
-        //     {
-        //         MethodInfo = requestDelegate.Method,
-        //         ApplicationServices = options.EndpointBuilder.ApplicationServices
-        //     };
-        //     var jsonTypeInfo = (JsonTypeInfo<object>)jsonSerializerOptions.GetReadOnlyTypeInfo(typeof(object));
-        //
-        //     EndpointFilterDelegate filteredInvocation = async (EndpointFilterInvocationContext context) =>
-        //     {
-        //         if (context.HttpContext.Response.StatusCode < 400)
-        //         {
-        //             await requestDelegate(context.HttpContext);
-        //         }
-        //         return EmptyHttpResult.Instance;
-        //     };
-        //
-        //     var initialFilteredInvocation = filteredInvocation;
-        //     for (var i = options.EndpointBuilder.FilterFactories.Count - 1; i >= 0; i--)
-        //     {
-        //         var currentFilterFactory = options.EndpointBuilder.FilterFactories[i];
-        //         filteredInvocation = currentFilterFactory(factoryContext, filteredInvocation);
-        //     }
-        //
-        //     // The filter factories have run without modifying per-request behavior, we can skip running the pipeline.
-        //     if (ReferenceEquals(initialFilteredInvocation, filteredInvocation))
-        //     {
-        //         return requestDelegate;
-        //     }
-        //
-        //     return async (HttpContext httpContext) =>
-        //     {
-        //         var obj = await filteredInvocation(new DefaultEndpointFilterInvocationContext(httpContext, new object[] { httpContext }));
-        //         if (obj is not null)
-        //         {
-        //             await ExecuteHandlerHelper.ExecuteReturnAsync(obj, httpContext, jsonTypeInfo);
-        //         }
-        //     };
-        // }
-#endif
     }
 }
