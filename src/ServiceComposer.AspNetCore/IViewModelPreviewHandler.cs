6d2787a3c2a1a875ef1a89dc63ed90231bacc0a0
Mauro Servienti <mauro.servienti@gmail.com>
2024-09-08 10:37:33 +0200

    Bump version to 3.0 (#713)
    * Bump version to 3.0
    
    * Remove all obsolete code in v3
    
    * improvement
    
    * Upgrade guide draft
    
    * Upgrade guide from v2.x to 3.0.0

--- src/ServiceComposer.AspNetCore/IViewModelPreviewHandler.cs
+++ src/ServiceComposer.AspNetCore/IViewModelPreviewHandler.cs
@@ -6,18 +6,6 @@ namespace ServiceComposer.AspNetCore
 {
     public interface IViewModelPreviewHandler
     {
-        [Obsolete("Use Preview(HttpRequest request). Will be treated as an error starting v2 and removed in v3.", error: true)]
-        Task Preview(dynamic viewModel)
-        {
-            throw new NotSupportedException();
-        }
-
-        [Obsolete("Use Preview(HttpRequest request). Will be treated as an error starting v2 and removed in v3.", error: true)]
-        Task Preview(HttpRequest request, dynamic viewModel)
-        {
-            throw new NotSupportedException();
-        }
-
         Task Preview(HttpRequest request);
     }
 }
