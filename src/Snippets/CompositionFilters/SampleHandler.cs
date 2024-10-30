235a7e6e46c098e473bc53f8deafdf773d3e6293
Mauro Servienti <mauro.servienti@gmail.com>
2024-10-30 12:27:46 +0100

    Code snippets

---
+++ src/Snippets/CompositionFilters/SampleHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceComposer.AspNetCore;
+
+namespace Snippets.CompositionFilters
+{
+    // begin-snippet: composition-filter-attribute
+    public class SampleCompositionFilterAttribute : CompositionRequestFilterAttribute
+    {
+        public override ValueTask<object> InvokeAsync(CompositionRequestFilterContext context, CompositionRequestFilterDelegate next)
+        {
+            return next(context);
+        }
+    }
+    // end-snippet
+    
+    // begin-snippet: composition-filter-class
+    public class SampleCompositionFilter : ICompositionRequestFilter<SampleHandler>
+    {
+        public ValueTask<object> InvokeAsync(CompositionRequestFilterContext context, CompositionRequestFilterDelegate next)
+        {
+            return next(context);
+        }
+    }
+    // end-snippet
+    
+    // begin-snippet: handler-with-composition-filter
+    public class SampleHandler : ICompositionRequestsHandler
+    {
+        [SampleCompositionFilter]
+        [HttpGet("/sample/{id}")]
+        public Task Handle(HttpRequest request)
+        {
+            return Task.CompletedTask;
+        }
+    }
+    // end-snippet
+}
