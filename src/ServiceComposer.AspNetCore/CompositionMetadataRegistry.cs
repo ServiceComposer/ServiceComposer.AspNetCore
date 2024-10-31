36055494b9ecc4a36624728cc0bb63e4fe702ae9
Mauro Servienti <mauro.servienti@gmail.com>
2024-10-31 11:07:09 +0100

    Register ICompositionEventsHandler in DI and in metadata registry

--- src/ServiceComposer.AspNetCore/CompositionMetadataRegistry.cs
+++ src/ServiceComposer.AspNetCore/CompositionMetadataRegistry.cs
@@ -5,11 +5,23 @@ namespace ServiceComposer.AspNetCore
 {
     class CompositionMetadataRegistry
     {
-        internal HashSet<Type> Components { get; } = new();
+        internal HashSet<Type> Components { get; } = [];
+        internal Dictionary<Type, List<Type>> EventHandlers = new();
 
         public void AddComponent(Type type)
         {
             Components.Add(type);
+        }
+
+        public void AddEventHandler(Type eventType, Type handlerType)
+        {
+            if (!EventHandlers.TryGetValue(eventType, out var handlers))
+            {
+                handlers = [];
+                EventHandlers.Add(eventType, handlers);
+            }
+            
+            handlers.Add(handlerType);
         }
     }
 }
