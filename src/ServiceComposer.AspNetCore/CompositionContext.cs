c8f9c42f14f9a42186bf5027f3afaf24254255f2
Mauro Servienti <mauro.servienti@gmail.com>
2024-10-31 11:08:15 +0100

    Use the new ICompositionEventsHandler handlers (suboptimal implementation)

--- src/ServiceComposer.AspNetCore/CompositionContext.cs
+++ src/ServiceComposer.AspNetCore/CompositionContext.cs
@@ -4,40 +4,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ServiceComposer.AspNetCore
 {
-    class CompositionContext : ICompositionContext, ICompositionEventsPublisher
+    class CompositionContext(string requestId, HttpRequest httpRequest, CompositionMetadataRegistry metadataRegistry)
+        : ICompositionContext, ICompositionEventsPublisher
     {
-        readonly RouteData _routeData;
-        readonly HttpRequest _httpRequest;
         readonly ConcurrentDictionary<Type, List<CompositionEventHandler<object>>> _compositionEventsSubscriptions = new();
 
-        public CompositionContext(string requestId, RouteData routeData, HttpRequest httpRequest)
-        {
-            _routeData = routeData;
-            _httpRequest = httpRequest;
-            RequestId = requestId;
-        }
-        public string RequestId { get; }
+        public string RequestId { get; } = requestId;
+
         public Task RaiseEvent(object @event)
         {
-            if (_compositionEventsSubscriptions.TryGetValue(@event.GetType(), out var compositionHandlers))
+            var handlers = new List<CompositionEventHandler<object>>();
+            if (metadataRegistry.EventHandlers.TryGetValue(@event.GetType(), out var handlerTypes))
             {
-                var tasks = compositionHandlers.Select(handler => handler.Invoke(@event, _httpRequest)).ToList();
+                handlerTypes.ForEach(handlerType =>
+                {
+                    Task EventHandler(object evt, HttpRequest req)
+                    {
+                        var handler = httpRequest.HttpContext.RequestServices.GetRequiredService(handlerType);
+                        var mi = handlerType.GetMethod(nameof(ICompositionEventsHandler<object>.Handle));
+                        var ret = mi.Invoke(handler, new[] { @event, req });
+                        return ret as Task;
+                    }
 
-                return Task.WhenAll(tasks);
+                    handlers.Add(EventHandler);
+                });
             }
 
-            return Task.CompletedTask;
+            if (_compositionEventsSubscriptions.TryGetValue(@event.GetType(), out var compositionHandlers))
+            {
+                handlers.AddRange(compositionHandlers);
+            }
+            
+            var tasks = handlers.Select(handler => handler.Invoke(@event, httpRequest)).ToList();
+            return Task.WhenAll(tasks);
         }
 
         public void Subscribe<TEvent>(CompositionEventHandler<TEvent> handler)
         {
             if (!_compositionEventsSubscriptions.TryGetValue(typeof(TEvent), out var handlers))
             {
-                handlers = new List<CompositionEventHandler<object>>();
+                handlers = [];
                 _compositionEventsSubscriptions.TryAdd(typeof(TEvent), handlers);
             }
 
