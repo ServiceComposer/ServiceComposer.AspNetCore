e284b68f1949e15d90b3468baea8a29519e35eec
Mauro Servienti <mauro.servienti@gmail.com>
2024-12-08 16:37:27 +0100

    Drop the obsolete EnableWriteSupport configuration option (#766)
    * Drop the obsolete EnableWriteSupport configuration option
    
    fix #654
    
    * Approved API

--- src/ServiceComposer.AspNetCore/ViewModelCompositionOptions-Obsoletes-V3.cs
+++ src/ServiceComposer.AspNetCore/ViewModelCompositionOptions-Obsoletes-V3.cs
@@ -1,12 +1,3 @@
 using System;
 
 namespace ServiceComposer.AspNetCore;
-
-partial class ViewModelCompositionOptions
-{
-    [Obsolete("EnableWriteSupport is obsolete. Starting v2.1.0, write support is enabled by default. Use DisableWriteSupport to disable it. It'll be considered an error in v3.0.0 and removed in v4.0.0", true)]
-    public void EnableWriteSupport()
-    {
-        throw new NotSupportedException();
-    }
-}
--- src/Snippets/WriteSupport/EnableWriteSupport.cs
+++ src/Snippets/WriteSupport/EnableWriteSupport.cs
@@ -4,17 +4,6 @@ using ServiceComposer.AspNetCore;
 
 namespace Snippets.WriteSupport;
 
-public class EnableWriteSupport
-{
-    [Obsolete("This snippet is used only by an upgrade guide. The Obsolete is needed to prevent the EnableWriteSupport usage here to cause the snippet compilation to fail.")]
-    // begin-snippet: enable-write-support
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddViewModelComposition(options => options.EnableWriteSupport());
-    } 
-    // end-snippet
-}
-
 public class DisableWriteSupport
 {
     // begin-snippet: disable-write-support
