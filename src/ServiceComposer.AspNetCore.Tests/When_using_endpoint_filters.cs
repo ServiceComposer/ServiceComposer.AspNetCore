4e06ac3e6fc69197888f21ce4f51a08f412c7f20
Mauro Servienti <mauro.servienti@gmail.com>
2024-12-28 11:01:50 +0100

    Add a failing test

--- src/ServiceComposer.AspNetCore.Tests/When_using_endpoint_filters.cs
+++ src/ServiceComposer.AspNetCore.Tests/When_using_endpoint_filters.cs
@@ -1,10 +1,15 @@
 #nullable enable
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using ServiceComposer.AspNetCore.Testing;
 using Xunit;
@@ -28,8 +33,6 @@ namespace ServiceComposer.AspNetCore.Tests
         
         class SampleEndpointFilter : IEndpointFilter
         {
-            public bool Invoked { get; set; }
-            public object CapturedResponse { get; set; }
             public bool Invoked { get; private set; }
             public object? CapturedResponse { get; private set; }
             
@@ -100,6 +103,99 @@ namespace ServiceComposer.AspNetCore.Tests
 
             Assert.True(anotherSampleEndpointFilter.Invoked);
             Assert.Equal(expectedComposedRequestId, (anotherSampleEndpointFilter.CapturedResponse as dynamic)?.RequestId);
+
+            var contentString = await response.Content.ReadAsStringAsync();
+            dynamic body = JObject.Parse(contentString);
+            Assert.Equal(expectedComposedRequestId, (string)body.RequestId);
+        }
+        
+        class ResponseHandlerWithModelBinding : ICompositionRequestsHandler
+        {
+            [HttpPost("/empty-response/{id}")]
+            [Model<ModelValues>]
+            public Task Handle(HttpRequest request)
+            {
+                var vm = request.GetComposedResponseModel();
+                var ctx = request.GetCompositionContext();
+                vm.RequestId = ctx.RequestId;
+
+                return Task.CompletedTask;
+            }
+        }
+
+        class ModelValues
+        {
+            [FromRoute(Name = "id")]
+            public int Identifier { get; set; }
+            
+            [FromBody]
+            public MyClass? Body { get; set; }
+        }
+
+        class MyClass
+        {
+            public required string Text { get; set; }
+        }
+
+        class CaptureArgumentsEndpointFilter : IEndpointFilter
+        {
+            public bool Invoked { get; private set; }
+            public IList<object?> CapturedArguments { get; private set; } = null!;
+
+            public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+            {
+                Invoked = true;
+                CapturedArguments = context.Arguments;
+                return await next(context);
+            }
+        }
+        
+        [Fact]
+        public async Task Should_populate_arguments_as_expected()
+        {
+            var expectedComposedRequestId = Guid.NewGuid().ToString();
+            var captureArgumentsEndpointFilter = new CaptureArgumentsEndpointFilter();
+
+            // Arrange
+            var client = new SelfContainedWebApplicationFactoryWithWebHost<When_using_endpoint_filters>
+            (
+                configureServices: services =>
+                {
+                    services.AddViewModelComposition(options =>
+                    {
+                        options.AssemblyScanner.Disable();
+                        options.ResponseSerialization.DefaultResponseCasing = ResponseCasing.PascalCase;
+                        options.RegisterCompositionHandler<ResponseHandlerWithModelBinding>();
+                    });
+                    services.AddRouting();
+                },
+                configure: app =>
+                {
+                    app.UseRouting();
+                    app.UseEndpoints(builder =>
+                    {
+                        builder.MapCompositionHandlers()
+                            .AddEndpointFilter(captureArgumentsEndpointFilter);
+                    });
+                }
+            ).CreateClient();
+
+            client.DefaultRequestHeaders.Add("composed-request-id", expectedComposedRequestId);
+
+            var modelBody = new MyClass(){ Text = "some text" };
+            var json = JsonConvert.SerializeObject(modelBody);
+            var stringContent = new StringContent(json, Encoding.UTF8, MediaTypeNames.Application.Json);
+            stringContent.Headers.ContentLength = json.Length;
+            
+            // Act
+            var response = await client.PostAsync("/empty-response/1", stringContent);
+
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.True(captureArgumentsEndpointFilter.Invoked);
+            
+            var arguments = captureArgumentsEndpointFilter.CapturedArguments;
+            Assert.NotNull(arguments);
+            Assert.True(arguments.Count == 2);
 
             var contentString = await response.Content.ReadAsStringAsync();
             dynamic body = JObject.Parse(contentString);
