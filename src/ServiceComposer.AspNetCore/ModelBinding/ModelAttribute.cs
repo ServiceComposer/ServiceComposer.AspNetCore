5bd287eb66e8231a1748523b3c2813734fff77ea
Mauro Servienti <mauro.servienti@gmail.com>
2024-12-29 15:41:17 +0100

    Define the initial attributes needed for declarative binding

--- src/ServiceComposer.AspNetCore/ModelBinding/ModelAttribute.cs
+++ src/ServiceComposer.AspNetCore/ModelBinding/ModelAttribute.cs
@@ -1,22 +1,26 @@
 #nullable enable
 using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ServiceComposer.AspNetCore;
 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public class ModelAttribute(Type type, ModelBindingSource bindingSource = ModelBindingSource.ModelBinding) : Attribute
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public abstract class ModelAttribute(Type type, BindingSource bindingSource) : Attribute
 {
-    public ModelBindingSource Source { get; } = bindingSource;
     public Type Type { get; } = type;
+    public BindingSource BindingSource { get; } = bindingSource;
+    public abstract string ModelName { get; }
     //public int Order { get; set; }
 }
 
-public enum ModelBindingSource
+public sealed class BindModelFromBodyAttribute<T>()
+    : ModelAttribute(typeof(T), BindingSource.Body)
 {
-    ModelBinding = 0,
-    Query,
-    Route,
-    Body,
-    Form,
-    Headers
+    public override string ModelName { get; } = "model";
+};
+
+public sealed class BindModelFromRouteAttribute<T>(string routeValueKey)
+    : ModelAttribute(typeof(T), BindingSource.Path)
+{
+    public override string ModelName { get; } = routeValueKey;
 }
