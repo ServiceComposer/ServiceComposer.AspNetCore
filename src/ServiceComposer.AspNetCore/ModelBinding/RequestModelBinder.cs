0e57bd6a75b9ae70f8a158c1d9e94bd0d4970f0d
Mauro Servienti <mauro.servienti@gmail.com>
2024-12-29 15:37:12 +0100

    Improve the binder logic

--- src/ServiceComposer.AspNetCore/ModelBinding/RequestModelBinder.cs
+++ src/ServiceComposer.AspNetCore/ModelBinding/RequestModelBinder.cs
@@ -39,26 +39,29 @@ namespace ServiceComposer.AspNetCore
             var valueProvider =
                 await CompositeValueProvider.CreateAsync(actionContext, mvcOptions.Value.ValueProviderFactories);
 
+            var bindingInfo = new BindingInfo()
+            {
+                BinderModelName = modelMetadata.BinderModelName,
+                BinderType = modelMetadata.BinderType,
+                BindingSource = modelMetadata.BindingSource,
+                PropertyFilterProvider = modelMetadata.PropertyFilterProvider,
+            };
+            
             var modelBindingContext = DefaultModelBindingContext.CreateBindingContext(
                 actionContext,
                 valueProvider,
                 modelMetadata,
-                bindingInfo: null,
+                bindingInfo: bindingInfo,
                 modelName: "");
 
+            // TODO remove this
             // modelBindingContext.Model = new T();
             modelBindingContext.PropertyFilter = _ => true; // All props
 
             var factoryContext = new ModelBinderFactoryContext()
             {
                 Metadata = modelMetadata,
-                BindingInfo = new BindingInfo()
-                {
-                    BinderModelName = modelMetadata.BinderModelName,
-                    BinderType = modelMetadata.BinderType,
-                    BindingSource = modelMetadata.BindingSource,
-                    PropertyFilterProvider = modelMetadata.PropertyFilterProvider,
-                },
+                BindingInfo = bindingInfo,
                 CacheToken = modelMetadata,
             };
 
@@ -71,13 +74,21 @@ namespace ServiceComposer.AspNetCore
                 modelBindingContext.ModelState);
         }
 
-        internal async Task<(object Model, bool IsModelSet, ModelStateDictionary ModelState)> TryBind(Type modelType,
-            HttpRequest request)
+        internal async Task<(object Model, bool IsModelSet, ModelStateDictionary ModelState)> TryBind(
+            Type modelType,
+            HttpRequest request,
+            string modelName,
+            BindingSource bindingSource)
         {
             //always rewind the stream; otherwise,
             //if multiple handlers concurrently bind
             //different models only the first one succeeds
             request.Body.Position = 0;
+            
+            // TODO remove this
+            // var reader = new StreamReader(request.Body);
+            // var content = await reader.ReadToEndAsync();
+            // request.Body.Position = 0;
 
             var modelMetadata = modelMetadataProvider.GetMetadataForType(modelType);
             var actionContext = new ActionContext(
@@ -88,26 +99,30 @@ namespace ServiceComposer.AspNetCore
             var valueProvider =
                 await CompositeValueProvider.CreateAsync(actionContext, mvcOptions.Value.ValueProviderFactories);
 
+            var bindingInfo = new BindingInfo()
+            {
+                BinderModelName = modelMetadata.BinderModelName,
+                BinderType = modelMetadata.BinderType,
+                BindingSource = bindingSource,
+                PropertyFilterProvider = modelMetadata.PropertyFilterProvider,
+            };
+            
             var modelBindingContext = DefaultModelBindingContext.CreateBindingContext(
                 actionContext,
                 valueProvider,
                 modelMetadata,
-                bindingInfo: null,
-                modelName: "");
+                bindingInfo: bindingInfo,
+                modelName: modelName);
 
+            // TODO remove this
             //modelBindingContext.Model = new T();
             modelBindingContext.PropertyFilter = _ => true; // All props
+            modelBindingContext.BindingSource = bindingSource;
 
             var factoryContext = new ModelBinderFactoryContext()
             {
                 Metadata = modelMetadata,
-                BindingInfo = new BindingInfo()
-                {
-                    BinderModelName = modelMetadata.BinderModelName,
-                    BinderType = modelMetadata.BinderType,
-                    BindingSource = modelMetadata.BindingSource,
-                    PropertyFilterProvider = modelMetadata.PropertyFilterProvider,
-                },
+                BindingInfo = bindingInfo,
                 CacheToken = modelMetadata,
             };
 
