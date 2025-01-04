68338a73ca2d3a6f0380ab26049cf358cb620048
Mauro Servienti <mauro.servienti@gmail.com>
2025-01-04 10:53:06 +0100

    Add a playgroung project to test the various generation scenarios

---
+++ src/HandlersSyntaxPlayground/CompositionHandlers/MySampleCompositionHandler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HandlersSyntaxPlayground.CompositionHandlers;
+
+public class MySampleCompositionHandler
+{
+    [HttpPost("/sample/{id}")]
+    public Task Post(int id)
+    {
+        return Task.CompletedTask;
+    }
+}
