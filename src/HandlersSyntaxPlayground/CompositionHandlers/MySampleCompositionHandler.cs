f28d6cbe0087676d10e7af3bc9b7931ebfd8ceef
Mauro Servienti <mauro.servienti@gmail.com>
2025-01-07 18:31:25 +0100

    Great step in the right direction, still tons of to-dos

--- src/HandlersSyntaxPlayground/CompositionHandlers/MySampleCompositionHandler.cs
+++ src/HandlersSyntaxPlayground/CompositionHandlers/MySampleCompositionHandler.cs
@@ -4,9 +4,14 @@ namespace HandlersSyntaxPlayground.CompositionHandlers;
 
 public class MySampleCompositionHandler
 {
-    [HttpPost("/sample/{id}")]
-    public Task Post(int id)
+    [HttpPost("/sample/{id}?val={v}")]
+    public Task Post(int id, string v, [FromBody]BodyClass body)
     {
         return Task.CompletedTask;
     }
+}
+
+public class BodyClass
+{
+    public string S { get; set; }
 }
