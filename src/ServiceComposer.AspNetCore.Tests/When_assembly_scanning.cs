7cfd1a10d7a7a147e5028ebf0895dfb807ba6c25
Mauro Servienti <mauro.servienti@gmail.com>
2025-01-30 18:53:08 +0100

    Add a to-do

--- src/ServiceComposer.AspNetCore.Tests/When_assembly_scanning.cs
+++ src/ServiceComposer.AspNetCore.Tests/When_assembly_scanning.cs
@@ -179,5 +179,7 @@ namespace ServiceComposer.AspNetCore.Tests
             // Assert
             response.EnsureSuccessStatusCode();
         }
+        
+        // TODO add a test to verify contract-less handlers are registered in DI 
     }
 }
