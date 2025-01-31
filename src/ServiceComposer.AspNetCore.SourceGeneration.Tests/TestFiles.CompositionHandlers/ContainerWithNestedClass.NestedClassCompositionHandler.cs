766348764e557974627be1ea639a5b79eb864dbd
Mauro Servienti <mauro.servienti@gmail.com>
2025-01-31 23:22:12 +0100

    Add source generators approval tests

---
+++ src/ServiceComposer.AspNetCore.SourceGeneration.Tests/TestFiles.CompositionHandlers/ContainerWithNestedClass.NestedClassCompositionHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceComposer.AspNetCore.SourceGeneration.Tests.TestFiles.CompositionHandlers;
+
+public class ContainerWithNestedClass
+{
+    public class NestedClassCompositionHandler
+    {
+        [ResponseCache(Duration = 10, Location = ResponseCacheLocation.Client), HttpPost("/sample/{v}")]
+        public Task Post([FromRoute(Name = "v")] int id, int c)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/ServiceComposer.AspNetCore.SourceGeneration.Tests/Tester.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace ServiceComposer.AspNetCore.SourceGeneration.Tests;
+
+static class StringExtensions
+{
+    public static string NormalizeLineEndings(this string text)
+    {
+        return text.Replace("\r\n", "\n").Replace("\r", "\n");
+    }
+}
+
+public class Tester
+{
+    static Compilation CreateCompilation(string source)
+    {
+        return CSharpCompilation.Create(
+            assemblyName: "TestAssembly",
+            syntaxTrees: [CSharpSyntaxTree.ParseText(source)],
+            references:
+            [
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
+            ],
+            options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+    }
+
+    [Fact]
+    public async Task TestSourceGenerator()
+    {
+        var testFilesPath = Path.Combine(Directory.GetCurrentDirectory(), "TestFiles.CompositionHandlers");
+        var allTestFiles = Directory.GetFiles(testFilesPath, "*.cs", SearchOption.TopDirectoryOnly);
+
+        var generator = new CompositionHandlerWrapperGenerator();
+        foreach (var testFile in allTestFiles)
+        {
+            var inputCode = File.ReadAllText(testFile);
+            var compilation = CreateCompilation(inputCode);
+            
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+            driver = driver.RunGeneratorsAndUpdateCompilation(compilation, 
+                out var outputCompilation, 
+                out var diagnostics);
+            
+            var runResult = driver.GetRunResult();
+            Assert.Empty(diagnostics);
+            
+            var generatedSource = runResult.GeneratedTrees[0].ToString();
+            
+            await Verify(generatedSource.NormalizeLineEndings())
+                .UseDirectory("ApprovedFiles")
+                .UseFileName(Path.GetFileName(testFile));
+        }
+    }
+}
