7168517f129b1840f3aac4f08eb17564ced5c925
Mauro Servienti <mauro.servienti@gmail.com>
2026-02-13 08:59:40 +0100

    Add a test to validate generic event handlers work with contract-less composition handlers and composition over controllers (#998)

--- src/ServiceComposer.AspNetCore.Tests/CompositionHandlers/When_using_composition_over_controllers_get_with_2_handlers.cs
+++ src/ServiceComposer.AspNetCore.Tests/CompositionHandlers/When_using_composition_over_controllers_get_with_2_handlers.cs
@@ -29,6 +29,12 @@ namespace ServiceComposer.AspNetCore.Tests.CompositionHandlers
         {
             [FromRoute]public int id { get; set; }
         }
+
+        public record AnEvent
+        {
+            
+        }
+ 
         public class CaseInsensitiveRouteTestGetIntegerCompositionHandler(IHttpContextAccessor httpContextAccessor)
         {
             [HttpGet("/api/compositionovercontrollerusingcompositionhandlers/{id}")]
@@ -58,8 +64,20 @@ namespace ServiceComposer.AspNetCore.Tests.CompositionHandlers
             [HttpGet("/api/CompositionOverControllerUsingCompositionHandlers/{id}")]
             public Task Handle()
             {
-                var vm = httpContextAccessor.HttpContext.Request.GetComposedResponseModel();
+                var request = httpContextAccessor.HttpContext!.Request;
+                var vm = request.GetComposedResponseModel();
                 vm.AString = "sample";
+                
+                return request.GetCompositionContext().RaiseEvent(new AnEvent());
+            }
+        }
+        
+        public class AnEventSubscriber : ICompositionEventsHandler<AnEvent>
+        {
+            public Task Handle(AnEvent @event, HttpRequest request)
+            {
+                var vm = request.GetComposedResponseModel();
+                vm.HandledAnEvent = true;
                 return Task.CompletedTask;
             }
         }
@@ -106,6 +124,54 @@ namespace ServiceComposer.AspNetCore.Tests.CompositionHandlers
             var responseString = await response.Content.ReadAsStringAsync();
             var responseObj = JObject.Parse(responseString);
 
+            Assert.Equal("sample", responseObj?.SelectToken("AString")?.Value<string>());
+            Assert.Equal(1, responseObj?.SelectToken("ANumber")?.Value<int>());
+        }
+        
+        [Fact]
+        public async Task Returns_expected_response_handling_anEvent()
+        {
+            // Arrange
+            var client = new SelfContainedWebApplicationFactoryWithWebHost<Dummy>
+            (
+                configureServices: services =>
+                {
+                    services.AddViewModelComposition(options =>
+                    {
+                        options.AssemblyScanner.Disable();
+                        options.RegisterCompositionHandler<TestGetStringCompositionHandler>();
+                        options.RegisterCompositionHandler<Generated.When_using_composition_over_controllers_get_with_2_handlers_TestGetStringCompositionHandler_Handle>();
+                        options.RegisterCompositionHandler<CaseSensitiveRouteTestGetIntegerCompositionHandler>();
+                        options.RegisterCompositionHandler<Generated.When_using_composition_over_controllers_get_with_2_handlers_CaseSensitiveRouteTestGetIntegerCompositionHandler_Handle_ServiceComposer_AspNetCore_Tests_CompositionHandlers_When_using_composition_over_controllers_get_with_2_handlers_Model_model>();
+                        options.RegisterCompositionHandler<AnEventSubscriber>();
+                        options.EnableCompositionOverControllers();
+                    });
+                    services.AddHttpContextAccessor();
+                    services.AddRouting();
+                    services.AddControllers()
+                            .AddNewtonsoftJson();
+                },
+                configure: app =>
+                {
+                    app.UseRouting();
+                    app.UseEndpoints(builder =>
+                    {
+                        builder.MapControllers();
+                        builder.MapCompositionHandlers();
+                    });
+                }
+            ).CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/compositionovercontrollerusingcompositionhandlers/1");
+
+            // Assert
+            Assert.True(response.IsSuccessStatusCode);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseObj = JObject.Parse(responseString);
+
+            Assert.True(responseObj.SelectToken("HandledAnEvent")?.Value<bool>());
             Assert.Equal("sample", responseObj?.SelectToken("AString")?.Value<string>());
             Assert.Equal(1, responseObj?.SelectToken("ANumber")?.Value<int>());
         }
